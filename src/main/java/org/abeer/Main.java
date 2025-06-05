c785896601d50db20150084ff1e9e2619e3a7a89
abeerrai01 <theabeerrai.gmail.com>
2025-06-05 13:50:19 +0530

    BANKACCOUNT USING JDBC

---
+++ src/main/java/org/abeer/Main.java
@@ -0,0 +1,88 @@
+package org.abeer;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main
+{
+    private static final String url="jdbc:mysql://localhost:3306/BANKACCOUNT";
+    private static final String username="root";
+    private static final String password="Swechhar";
+    public static void main(String[] args)
+    {
+       try
+       {
+           Connection conn=DriverManager.getConnection(url,username,password);
+           conn.setAutoCommit(false);
+           Scanner in=new Scanner(System.in);
+           System.out.println("ENTER ACCOUNT NUMBER TO DEBIT");
+           int acno=in.nextInt();
+           System.out.println("ENTER AMOUNT TO BE DEBITED");
+           double amount=in.nextDouble();
+           if (!CheckBalance(conn, acno, amount)) {
+               System.out.println("Insufficient balance ❌");
+               conn.rollback();
+               return;
+           }
+           String credit="UPDATE ACCOUNTS SET BALANCE=BALANCE-? WHERE ACCOUNTNUMBER=?;";
+           PreparedStatement cre=conn.prepareStatement(credit);
+           cre.setDouble(1,amount);
+           cre.setInt(2,acno);
+           int rowAffected=cre.executeUpdate();
+           if(rowAffected<=0)
+           {
+               System.out.println("Debit failed ❌");
+               conn.rollback();
+               return;
+           }
+           String debit="UPDATE ACCOUNTS SET BALANCE=BALANCE+? WHERE ACCOUNTNUMBER=?;";
+           PreparedStatement deb=conn.prepareStatement(debit);
+           System.out.println("ENTER ACCOUNT NUMBER TO CREDIT");
+           int acno2=in.nextInt();
+           deb.setDouble(1,amount);
+           deb.setInt(2,acno2);
+           int rowAffected2=deb.executeUpdate();
+           if(rowAffected2<=0)
+           {
+               System.out.println("Credit failed ❌");
+               conn.rollback();
+               return;
+           }
+           conn.commit();
+           System.out.println("Transaction successful ✅");
+
+       }
+       catch (SQLException e)
+       {
+           System.out.println(e);
+       }
+    }
+    public static boolean CheckBalance(Connection conn,int acno,double amount)
+    {
+        try
+        {
+            String query="SELECT BALANCE FROM ACCOUNTS WHERE ACCOUNTNUMBER=?";
+            PreparedStatement ps=conn.prepareStatement(query);
+            ps.setInt(1,acno);
+            ResultSet rs=ps.executeQuery();
+            double balance=-1;
+            while(rs.next())
+            {
+                balance=rs.getDouble("BALANCE");
+            }
+            if(balance>=amount)
+            {
+                conn.commit();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+        return true;
+    }
+}
