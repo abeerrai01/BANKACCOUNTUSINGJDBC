0e4e48a3533e1f20ec5ad442983938289c095bfc
abeerrai01 <theabeerrai.gmail.com>
2025-06-12 18:27:10 +0530

    Removed Credits.java from tracking

--- src/main/java/org/abeer/Confidential/Credits.java
+++
@@ -1,8 +0,0 @@
-package org.abeer.Confidential;
-
-public class Credits
-{
-    public static final String url="jdbc:mysql://localhost:3306/BANKACCOUNT";
-    public static final String username="root";
-    public static final String password="Swechhar";
-}
